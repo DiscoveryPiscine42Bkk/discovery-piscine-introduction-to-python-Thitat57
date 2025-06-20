096724a80c5fd2dfa2bd11f2cd54959c847cc127
thitsawa <67110032@kmitl.ac.th>
2025-06-20 12:13:26 +0000

    cell03

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,17 @@
+#!/home/codespace/.python/current/bin/python3
+import sys
+
+text_input = input("Enter a number less than 25\n")
+try:
+    number = int(text_input)
+except ValueError:
+    print("Invalid Input!")
+    sys.exit()
+
+if (number > 25):
+    print("Error")
+    sys.exit()
+
+while (number <= 25):
+    print(f"Inside the loop, my variable is {number}")
+    number += 1
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,14 @@
+#!/home/codespace/.python/current/bin/python3
+import sys
+
+text_input = input("Enter a number\n")
+try:
+    number = int(text_input)
+except ValueError:
+    print("Invalid Input!")
+    sys.exit()
+
+start = 0
+while (start <= 12):
+    print(f"{start} x {number} = {start*number}")
+    start += 1
